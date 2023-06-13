0a179fafcdb7e9b9b9ad4f3a55abb0189c5f4695
marcelYepes <marcelyepesqf@gmail.com>
2023-06-13 10:10:07 -0500

    adding backend

--- src/hooks/useUserCrud.js
+++ src/hooks/useUserCrud.js
@@ -1,49 +1,47 @@
 import axios from "axios"
-import { useState } from "react"
+import {useState} from "react"
+import {URL_API} from "../../url"
 
 const useUserCrud = () => {
+  const [users, setUsers] = useState()
+  const url = URL_API.url
+  const getAllUsers = () => {
+    axios
+      .get(`${url}/users`)
+      .then(res => setUsers(res.data))
+      .catch(err => console.log(err))
+  }
 
-const [users, setUsers] = useState()
+  const createNewUser = data => {
+    axios
+      .post(`${url}/users`, data)
+      .then(res => getAllUsers())
+      .catch(err => console.log(err))
+  }
 
-const url = 'https://users-crud.academlo.tech/users/'
+  const deleteUserById = id => {
+    const urlDelete = `${url}/users/${id}`
+    axios
+      .delete(urlDelete)
+      .then(res => getAllUsers())
+      .catch(err => console.log(err))
+  }
 
-const getAllUsers = () =>{
-axios.get(url)
-    .then(res => setUsers(res.data))
-    .catch(err => console.log(err))
-}
+  const updateUserById = (id, data) => {
+    const urlUpdate = `${url}/users/${id}`
+    axios
+      .put(urlUpdate, data)
+      .then(res => getAllUsers())
+      .catch(err => console.log(err))
+  }
 
-const createNewUser = data => {
-    axios.post(url, data)
-    .then(res => getAllUsers())
-    .catch(err => console.log(err))
-}
-
-const deleteUserById = id => {
-    const urlDelete = `${url}${id}/`
-    axios.delete (urlDelete)
-    .then(res => getAllUsers())
-    .catch(err => console.log(err))
-}
-
-const updateUserById = (id, data) => {
-    const urlUpdate = `${url}${id}/`
-    axios.patch(urlUpdate, data)
-    .then(res => getAllUsers())
-    .catch(err => console.log(err))
-
-}
-
-return {
+  return {
     users,
     getAllUsers,
     createNewUser,
     deleteUserById,
-    updateUserById
-
-}
-
-
+    updateUserById,
+  }
 }
 
 export default useUserCrud
---
+++ url.js
@@ -0,0 +1,3 @@
+export const URL_API = {
+  url: "https://user-crud-my-academlo.onrender.com/api/v1",
+}
